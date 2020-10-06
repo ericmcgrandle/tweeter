fc202b82bbe0a3fa97622d5de4ff2734976019de
Eric McGrandle <ericmcgrandle@gmail.com>
2020-10-06 04:51:47 +0000

    day 1

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,18 @@
+$(document).ready(function() {
+
+  $('#tweet-text').on("input", function() {
+    const length = $(this).val().length; //Length of tweet
+    const remChars = 140 - length;
+
+    //this -> div class('tweet-text') -> form -> output('count')
+    $($(this)[0].parentElement.parentElement[2]).text(remChars);   
+    
+
+    //CHANGE SO JS MANIPULATES CSS?
+    if (remChars < 0) {
+      $('#count').css('color', '#ff0000');
+    } else {
+      $('#count').css('color', '#000000');
+    }
+  });
+});
