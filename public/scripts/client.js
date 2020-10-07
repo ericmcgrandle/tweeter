272c3d3dc3e98ba5916879b54920ef05e360e607
Eric McGrandle <ericmcgrandle@gmail.com>
2020-10-07 04:01:51 +0000

    post to tweets db

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,79 +1,51 @@
 $(document).ready(function() {
 
-const createTweetElement = function (obj) {
+  const createTweetElement = function (obj) {
+    const newTweet = `
+    <article class="old-tweet">
+      <header>
+        <div>
+          <img src="${obj.user.avatars}" alt="avatar">
+          <h6>${obj.user.name}</h6>
+        </div>
+        <div>
+          <h6 class="show-username">${obj.user.handle}</h6>
+        </div>
+      </header>
+      <p>
+        ${obj.content.text}
+      </p>
+      <footer>
+        <div>
+          10 days ago
+        </div>
+        <ul>
+          <li>share</li>
+          <li>flag</li>
+          <li>like</li>
+        </ul>
+      </footer>
+    </article>`
+    return newTweet;
+  };
 
-  const newTweet = `
-  <article class="old-tweet">
-    <header>
-      <div>
-        <img src="${obj.user.avatars}" alt="avatar">
-        <h6>${obj.user.name}</h6>
-      </div>
-      <div>
-        <h6 class="show-username">${obj.user.handle}</h6>
-      </div>
-    </header>
+  //Loop array and append to tweet container
+  const renderTweets = function (arr) {
+    arr.forEach((obj) => {
+      const $tweet = createTweetElement(obj);
+      $('#tweets-container').append($tweet); 
+    });
+  };
 
-    <p>
-      ${obj.content.text}
-    </p>
-
-    <footer>
-      <div>
-        10 days ago
-      </div>
-      <ul>
-        <li>share</li>
-        <li>flag</li>
-        <li>like</li>
-      </ul>
-    </footer>
-  </article>`
-
-  return newTweet;
-
-};
-
-
-const renderTweets = function (arr) {
-
-  arr.forEach((obj) => {
-    const $tweet = createTweetElement(obj);
-    $('#tweets-container').append($tweet); 
+  //Post method to tweets db
+  $('#post-tweet').submit(function(event) {
+    event.preventDefault();
+    const data = $(this).serialize();
+    $.post('/tweets', data, function(data, status) {
+      console.log('success');
+    });  
   });
 
-  
-};
 
-
-
-
-// Test / driver code (temporary). Eventually will get this from the server.
-const data = [
-  {
-    "user": {
-      "name": "Newton",
-      "avatars": "https://i.imgur.com/73hZDYK.png"
-      ,
-      "handle": "@SirIsaac"
-    },
-    "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
-    },
-    "created_at": 1461116232227
-  },
-  {
-    "user": {
-      "name": "Descartes",
-      "avatars": "https://i.imgur.com/nlhLi3I.png",
-      "handle": "@rd" },
-    "content": {
-      "text": "Je pense , donc je suis"
-    },
-    "created_at": 1461113959088
-  }
-]
-
-renderTweets(data);
 
 });
